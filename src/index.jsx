4fa22d1cefc9c6348a6d82a7c7284bc21fd7f3e4
Max Feskov <maxfeskov@gmail.com>
2021-03-09 02:25:12 +0300

    add store

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+import configureStore from './store';
+
 import App from './App';
 
 import './index.scss';
 
+const store = configureStore();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'),
 );
