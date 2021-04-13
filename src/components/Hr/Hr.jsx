4c37f057519a208fe26bf94b25b2041fa1fea1e1
Max Feskov <maxfeskov@gmail.com>
2021-04-13 17:10:34 +0300

    add Hr component

--- src/components/Hr/Hr.jsx
+++ src/components/Hr/Hr.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-function Hr() {
-  return <hr className="w-full border-t border-gray-600 my-4" />;
-}
+const Hr = () => <hr className="w-full border-t border-gray-600 my-4" />;
 
 export default Hr;
