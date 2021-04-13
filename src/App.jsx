36ce82f38e5402143d476c381fee730a17e05c51
Max Feskov <maxfeskov@gmail.com>
2021-04-13 17:28:15 +0300

    update App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import SearchForm from './components/SearchForm/SearchForm';
 import CryptoList from './components/CryptoList/CryptoList';
 import CryptoItemDiagram from './components/CryptoItemDiagram/CryptoItemDiagram';
-import Hr from './components/Hr/Hr';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import GlobalErrors from './components/GlobalErrors/GlobalErrors';
 
 function App() {
   return (
-    <div className="container mx-auto flex flex-col items-center bg-gray-100 p-4">
-      <div className="container">
-        <SearchForm />
-        <Hr />
-        <CryptoList />
-        <Hr />
-        <CryptoItemDiagram />
+    <>
+      <div className="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+        <div className="container">
+          <ErrorBoundary>
+            <SearchForm />
+            <CryptoList />
+            <CryptoItemDiagram />
+          </ErrorBoundary>
+        </div>
       </div>
-    </div>
+      <GlobalErrors />
+    </>
   );
 }
 
