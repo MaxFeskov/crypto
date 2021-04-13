59f1a8e964ff1274187872e9b2ab55ea28479ea4
Max Feskov <maxfeskov@gmail.com>
2021-04-13 17:00:29 +0300

    update store config

--- src/store.js
+++ src/store.js
@@ -1,13 +1,17 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import thunkMiddleware from 'redux-thunk';
+
+import globalErrors from './reducers/globalErrors';
+import coins from './reducers/coins';
+import api from './api';
 
 const composeEnhancers =
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
     : compose;
 
-const enhancer = composeEnhancers(applyMiddleware(thunk));
+const enhancer = composeEnhancers(applyMiddleware(thunkMiddleware.withExtraArgument(api)));
 
-const configureStore = () => createStore(combineReducers({}), enhancer);
+const configureStore = () => createStore(combineReducers({ globalErrors, coins }), enhancer);
 
 export default configureStore;
